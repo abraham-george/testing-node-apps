6ff5492bf4037a3cb100559414aef7a69f8cd22b
abraham-george <abraham.george.j@gmail.com>
2021-04-06 16:23:47 -0500

    Adding jest tests for pure function

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,10 +1,10 @@
 // Testing Pure Functions
 
 // 💣 remove this todo test (it's only here so you don't get an error about missing tests)
-test.todo('remove me')
+// test.todo('remove me')
 
 // 🐨 import the function that we're testing
-// 💰 import {isPasswordAllowed} from '../auth'
+import { isPasswordAllowed } from '../auth'
 
 // 🐨 write tests for valid and invalid passwords
 // 💰 here are some you can use:
@@ -19,3 +19,55 @@ test.todo('remove me')
 // - abc123! // no uppercase letters
 // - ABC123! // no lowercase letters
 // - ABCdef123 // no non-alphanumeric characters
+
+test('Valid Password', () => {
+    const isPassword = isPasswordAllowed('!aBc123')
+    expect(isPassword).toBe(true)
+})
+
+test('Invalid Password -> too short', () => {
+    const isPassword = isPasswordAllowed('a2c!')
+    expect(isPassword).toBe(false)
+})
+
+test('Invalid Password -> no alphabet characters', () => {
+  const isPassword = isPasswordAllowed('123456!')
+  expect(isPassword).toBe(false)
+})
+
+test('Invalid Password -> no numbers', () => {
+  const isPassword = isPasswordAllowed('ABCdef!')
+  expect(isPassword).toBe(false)
+})
+
+test('Invalid Password -> no uppercase letters', () => {
+  const isPassword = isPasswordAllowed('abc123!')
+  expect(isPassword).toBe(false)
+})
+
+test('Invalid Password -> no lowercase letters', () => {
+  const isPassword = isPasswordAllowed('ABC123!')
+  expect(isPassword).toBe(false)
+})
+
+test('Invalid Password -> no non-alphanumeric letters', () => {
+  const isPassword = isPasswordAllowed('ABCdef123')
+  expect(isPassword).toBe(false)
+})
+
+describe('Invalid Password Variations', () => {
+  test.each`
+    input          | expectedResult
+    ${'a2c!'}      | ${false}
+    ${'123456!'}   | ${false}
+    ${'ABCdef!'}   | ${false}
+    ${'abc123!'}   | ${false}
+    ${'ABC123!'}   | ${false}
+    ${'ABCdef123'} | ${false}
+  `(
+    'Testing various invalid password combinations',
+    ({input, expectedResult}) => {
+      expect(isPasswordAllowed(input)).toBe(expectedResult)
+    },
+  )
+})
